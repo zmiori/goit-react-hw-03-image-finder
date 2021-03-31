4dd4f7872d3fb8c1abef8e2a999906a4d6d64709
Zoe Baletska <zoya.miori@gmail.com>
2021-03-31 18:02:56 +0300

    finished services

--- src/services/api.js
+++ src/services/api.js
@@ -1,41 +1,27 @@
-const axios = require('axios').default;
+import axios from 'axios';
 
-const token = '19908670-1dd1b8415290a1f574a86bd8e';
-axios.defaults.baseURL = 'https://pixabay.com/api';
+const API_KEY = '19908670-1dd1b8415290a1f574a86bd8e';
+const BASE_URL = 'https://pixabay.com/api';
 
-// axios.default.params = {
-//   key: token,
-//   image_type: 'photo',
-//   orientation: 'horisontal',
-//   per_page: 12,
-// };
+axios.defaults.baseURL = BASE_URL;
+axios.defaults.params = {
+  key: API_KEY,
+  image_type: 'photo',
+  orientation: 'horisontal',
+  per_page: 12,
+};
 
-// const fetchImages = async ({ searchQuery, currentPage }) => {
-//   try {
-//     const { data } = await axios.get('', {
-//       params: { searchQuery, currentPage },
-//     });
-
-//     return data.hits;
-//   } catch (error) {
-//     console.log('error', { error });
-//     return [];
-//   }
-// };
-
-const fetchImages = ({ searchQuery, currentPage = 1, pageSize = 12 }) => {
-  console.log(searchQuery);
-
-  return axios
-    .get(
-      `https://pixabay.com/api/?q=${searchQuery}&page=${currentPage}&key=${token}&image_type=photo&orientation=horizontal&per_page=${pageSize}
-`,
-    )
-    .then(response => response.data.hits)
-    .catch(function (error) {
-      console.log(error);
-      return [];
+const fetchImages = async ({ searchQuery, currentPage }) => {
+  try {
+    const { data } = await axios.get('', {
+      params: { q: searchQuery, page: currentPage },
     });
+
+    return data.hits;
+  } catch (error) {
+    console.log('error', { error });
+    return [];
+  }
 };
 
 export default fetchImages;
